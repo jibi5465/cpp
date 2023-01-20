d72ac16323000ae0188f01ddf19062051f54f151
sakdipat <sakdipat5465@gmail.com>
2023-01-20 17:16:08 +0700

    first step for cpp

---
+++ c++01/function01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void function(){
+    cout << "Hello from function()" << endl;
+}
+
+int main(){
+    cout << "Hello from main()\n";
+    function();
+
+    
+    system("pause>0");
+}
--- cpp01.cpp
+++ cpp01.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "first step" ;
+    cout << "Hello World"<<endl;
+    string fruit = "apple";
+    string fruits[5] = {"apple","orange","grape","pear","banana"};
+    for (int i = 0; i < 5; ++i){
+        cout << fruits[i]<<endl;
+    } 
+
+    return 0;
 }
