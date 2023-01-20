a2077e396b2a37b136878940401eaa4866d5ee9a
sakdipat <sakdipat5465@gmail.com>
2023-01-20 18:11:28 +0700

    finish class01

---
+++ Myfirstapp(Codebeauty)/CB01200166.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World\n"; //this will output to the console hello world , \n mean endline
+    cout << "My name is Sakdipat.";
+    cout << "Today is Friday."<<endl; //endl is important if not have code will not run
+    cout << "Have a nice day."; //run in vscode not process but in cmd it can
+    
+    system("pause>0");
+}
