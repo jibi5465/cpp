c49227d02dbedda380f6398d6e54f00e99b1fec2
sakdipat <sakdipat5465@gmail.com>
2023-02-22 07:36:10 +0700

    new class

---
+++ Myfirstapp(Codebeauty)08/CB09220266.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+    //BMI calculator
+    //weight(kg) / height(m) * height(m)
+    //Underweight < 18.5
+    //Normal weight 18.5-24.9
+    //Overweight > 25
+
+    
+
+    system("pause > 0");
+}
