eafb73da926a0e427405e28aa203d4186086d3ea
sakdipat <sakdipat5465@gmail.com>
2023-02-23 18:02:24 +0700

    end class

--- Myfirstapp(Codebeauty)08/CB09220266.cpp
+++ Myfirstapp(Codebeauty)08/CB09220266.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-void main()
+int main()
 {
     //BMI calculator
     //weight(kg) / height(m) * height(m)
@@ -9,7 +9,26 @@ void main()
     //Normal weight 18.5-24.9
     //Overweight > 25
 
+    float weight, height, bmi;
+    cout << "Please enter your weight" << endl;
+    cin >> weight;
+    cout << "weight(Kg) : " << weight << endl;
+    cout << "Please enter your height" << endl;
+    cin >> height;
+    cout << "height(Kg) : " << height << endl;
     
+
+    bmi = weight / (height * height) * 10000;
+    
+    if (bmi < 18.5){
+        cout << "Underweight" << endl;
+    } else if (bmi >= 18.5 && bmi <= 24.9){
+        cout << "Normalweight"<< endl;
+    } else {
+        cout << "Overweight" << endl;
+    }
+
+    cout << "Your bmi is " << bmi << endl;
 
     system("pause > 0");
 }
