6b1cbe2fef48c50aedd518cc807b6ee6b5e0a1c9
sakdipat <sakdipat5465@gmail.com>
2023-02-24 19:45:40 +0700

    end class calculator

---
+++ Myfirstapp(Codebeauty)10/CB11240266.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    float num1,num2;
+    char operation;
+    cout << "**CodeBeauty Calculator**" << endl;
+    cin >> num1 >> operation >> num2;
+
+    switch (operation){
+        case '-':cout << num1 << operation << num2 << "=" << num1 - num2 ; break;
+        case '+':cout << num1 << operation << num2 << "=" << num1 + num2 ; break;
+        case '*':cout << num1 << operation << num2 << "=" << num1 * num2 ; break;
+        case '/':cout << num1 << operation << num2 << "=" << num1 / num2 ; break;
+        case '%':
+            bool isNum1Int, isNum2Int;
+            isNum1Int=((int)num1 == num1); //5==5.0
+            isNum2Int=((int)num2 == num2);
+
+            if(isNum1Int==true && isNum2Int==true)
+                cout << num1 << operation << num2 << "=" << (int)num1 % (int)num2 ;
+                else
+                    cout << "Not valid!";
+                        break;
+
+        default:cout << "Not valid operation!" << endl; break;
+    }
+
+    system("pause > 0");
+}
