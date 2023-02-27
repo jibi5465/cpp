be19a4a7ebcb253d85563986a079e452ec71ddf2
sakdipat <sakdipat5465@gmail.com>
2023-02-27 19:16:15 +0700

    end class

--- Myfirstapp(Codebeauty)16/CB17270266.cpp
+++ Myfirstapp(Codebeauty)16/CB17270266.cpp
@@ -3,7 +3,29 @@ using namespace std;
 
 int main(){
 
+    // The factorial of a number
+
+    int number;
+    cout << "Number: ";
+    cin >> number;
+    int factorial = 1;
+
+    //6!=1*2*3*4*5*6=720
+    for(int i = 1; i <= number;i++){
+        cout << "i = " << i << endl;
+        factorial = factorial*i;
+        cout << i << "! = " << factorial << endl;
+    }
     
+    //6! = 6*5*4*3*2*1
+    /*
+    for (int i = number; i >= 1; i--){
+        cout << "i = " << i << endl;
+        factorial = factorial*i;
+    }
+    */
+
+    cout << number << "! = " << factorial << endl; 
 
     system("pause > 0");
 }
