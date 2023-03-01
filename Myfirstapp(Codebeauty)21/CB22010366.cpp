29a17e222f868903531343dbc933e5b5600dae3c
sakdipat <sakdipat5465@gmail.com>
2023-03-01 20:05:12 +0700

    end class what's position of function

---
+++ Myfirstapp(Codebeauty)21/CB22010366.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void function();
+
+int main(){
+
+    cout << "Hello from main()\n" ;
+    function();
+
+    system("pause > 0");
+}
+
+void function(){
+    cout << "Hello from function()" << endl;
+}
